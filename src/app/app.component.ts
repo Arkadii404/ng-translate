4191c728896ad935d637dbdf9fc6f6af11179cda
Arkadii <veretilin1@gmail.com>
2021-06-21 21:07:34 +0300

    Created app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-translate';
+  public langs = ['en', 'ru'];
+
+  public setLang(lang: string): void {
+    this.translate.setDefaultLang(lang);
+    this.translate.use(lang);
+  }
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
